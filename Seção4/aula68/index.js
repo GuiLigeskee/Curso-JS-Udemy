003285cda00c31247ba36ae80b3f8ee36ac52cc5
GuiLigeskee <112131340+GuiLigeskee@users.noreply.github.com>
2023-01-10 20:15:17 -0300

    .

---
+++ Seção4/aula68/index.js
@@ -0,0 +1,9 @@
+//As várias maneiras de declarar funções em JS
+
+
+//declaração de função (Function hoisting) -- a função pode ser declarada antes ou depois de ser chamada que não vai dar erro;
+falaOi();
+
+function falaOi(){
+    console.log('Oie')
+}
